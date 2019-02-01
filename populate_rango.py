db6cff76e195c16472a0136339d0d7d8f30c3d27
unknown <2358931p@student.gla.ac.uk>
2019-02-01 23:36:29 +0000

    end of Chapter6_1

--- populate_rango.py
+++ populate_rango.py
@@ -35,7 +35,7 @@ def populate():
 		{"title":"Flask",
 		"url":"http://flask.pocoo.org"} ]
 
-	cats = {"Python": {"pages": python_pages,"views":164,"likes":64},
+	cats = {"Python": {"pages": python_pages,"views":128,"likes":64},
 			"Django": {"pages": django_pages,"views":64,"likes":32},
 			"Other Frameworks": {"pages": other_pages,"views":32,
 			"likes":16},
--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 def index(request):
 	category_list=Category.objects.order_by('-likes')[:5]
 	view_list=Page.objects.order_by('-views')[:5]
-	context_dict={'categories': category_list,'views':view_list}
+	context_dict={'categories': category_list,'viewnum':view_list}
 	return render(request,'rango/index.html',context=context_dict)
 	#return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
@@ -26,3 +27,28 @@ def show_category(request,category_name_slug):
 		context_dict['pages'] = None
 		
 	return render(request,'rango/category.html',context_dict)
+	
+def add_category(request):
+	form=CategoryForm()
+	if request.method=='POST':
+		form=CategoryForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+	return render(request,'rango/add_category.html',{'form':form})
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
